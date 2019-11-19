5ee33ac8f97bc3975e4ac8c7e384ff4b4e0f877d
Jaman Dedy <jeandedieuam@gmail.com>
2019-11-19 19:43:16 +0200

    basic burger

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
-const toolbar =() =>(
-    <header>
-        <div>MENU</div>
-        <div>LOGO</div>
-        <nav>
-            ....links
-        </nav>
-    </header>
-)
+const toolbar = () => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      {" "}
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
+);
 
-export default toolbar
+export default toolbar;
