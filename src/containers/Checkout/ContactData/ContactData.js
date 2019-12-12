0b3a5f41e7ae86c84462553927f19284880df9cb
Jaman Dedy <jeandedieuam@gmail.com>
2019-12-12 12:57:55 +0200

    adding dynamic input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -57,9 +57,9 @@ class ContactData extends Component {
           ]
         },
         value: ""
-      },
-      loading: false
-    }
+      }
+    },
+    loading: false
   };
   orderHandler = event => {
     event.preventDefault();
@@ -79,27 +79,23 @@ class ContactData extends Component {
       });
   };
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
     let form = (
       <form>
-        <Input elementType="..." elementConfig="..." value="" />
-        <Input
-          inputtype="input"
-          type="email"
-          name="email"
-          placeholder="Your Email"
-        />
-        <Input
-          inputtype="input"
-          type="text"
-          name="street"
-          placeholder="Your Street"
-        />
-        <Input
-          inputtype="input"
-          type="text"
-          name="postal"
-          placeholder="Postal Code"
-        />
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
         <Button clicked={this.orderHandler} btnType="Success">
           ORDER
         </Button>
